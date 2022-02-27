b7bf2214413f85d4dcf82e02b73cb7994e738541
yaara2427 <yaara2427@gmail.com>
2022-02-27 15:43:14 +0200

    ff

---
+++ src/geometries/Geometry.java
@@ -0,0 +1,9 @@
+package geometries;
+
+import primitives.Point;
+
+public interface Geometry {
+    public Vector getNormal(Point p) {
+
+    }
+}
--- src/geometries/Polygon.java
+++ src/geometries/Polygon.java
@@ -88,5 +88,3 @@ public class Polygon implements Geometry {
     public Vector getNormal(Point point) {
         return plane.getNormal();
     }
-            ityfkh,gklkjjhkjjklkjhkjh
-kkjhgkjhgkjhgkjhgkjhgkjhg
