e4a0580e7e345faa0e9849917d95b7d2d5fb1a28
yaara2427 <yaara2427@gmail.com>
2022-02-27 16:01:15 +0200

    ff

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -1,4 +1,18 @@
 package geometries;
 
+import primitives.Point;
+import primitives.Vector;
+
 public class Plane {
+    Point p0;
+    Vector normal;
+
+    public Plane(Point p1, Point p2, Point p3) {
+
+    }
+
+    public Plane(Point _p0, Vector _normal) {
+        p0= _p0;
+        normal=_normal.normalize();
+    }
 }
