29124dfab7b9b613e10c30d6e3bd170e545a23c3
yaara2427 <yaara2427@gmail.com>
2022-02-27 15:19:49 +0200

    ff

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,5 +1,37 @@
 package primitives;
 
 public class Ray {
+    Point p0;
+    Vector dir;
+
+    public Ray(Point _p0, Vector _dir) {
+        p0 = _p0;
+        dir = _dir.normalize();
+    }
+
+    @Override
+    public String toString() {
+        return "Ray{" +
+                "p0=" + p0 +
+                ", dir=" + dir +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ray ray = (Ray) o;
+
+        if (!p0.equals(ray.p0)) return false;
+        return dir.equals(ray.dir);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = p0.hashCode();
+        result = 31 * result + dir.hashCode();
+        return result;
+    }
 }
-ll
