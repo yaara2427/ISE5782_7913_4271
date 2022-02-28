93bb21747b09e4119312abe6cb3aaca9ebe73ffd
Tehila321 <100318288+Tehila321@users.noreply.github.com>
2022-02-28 20:14:23 +0200

    Update Sphere.java

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -1,4 +1,34 @@
 package geometries;
+import java.util.List;
 
-public class Sphere {
+import primitives.*;
+import static primitives.Util.*;
+public class Sphere implements Geometry {
+    private Point _center;
+    private double _radius;
+    public Sphere(Point p, double radius)
+    {
+        _center=p;
+        _radius =  radius;
+    }
+    @Override
+    public Vector getNormal(Point p) {
+        Vector v = p.subtract(_center);
+        return v.normalize();
+    }
+
+    @Override
+    public String toString() {
+        return "Sphere: " +
+                "center: " + _center +
+                "radius: " + _radius;
+    }
+    public Point getCenter()
+    {
+        return _center;
+    }
+    public double getRadius()
+    {
+        return _radius;
+    }
 }
