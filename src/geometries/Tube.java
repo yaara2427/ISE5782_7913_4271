d07157d2dcb2e3629a43bea5e6820a4c8b1dc40b
Tehila321 <100318288+Tehila321@users.noreply.github.com>
2022-03-01 18:45:37 +0200

    Update Tube.java

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -1,4 +1,19 @@
 package geometries;
+import java.util.List;
 
-public class Tube {
+import primitives.*;
+import static primitives.Util.*;
+
+public class Tube implements Geometry {
+    protected Ray _axisRay;
+    protected double _radius;
+
+    public Tube(Ray axisRay, double radius) {
+        _axisRay = axisRay;
+        _radius = radius;
+    }
+    @Override
+    public Vector getNormal(Point point) {
+        return null;
+    }
 }
