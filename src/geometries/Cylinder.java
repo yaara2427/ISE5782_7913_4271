754a3cb866e0a502765606200d0ef6d2758e7b6e
Tehila321 <100318288+Tehila321@users.noreply.github.com>
2022-03-02 11:23:24 +0200

    Update Cylinder.java

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -1,4 +1,22 @@
 package geometries;
+import java.util.List;
 
-public class Cylinder {
+import primitives.*;
+import static primitives.Util.*;
+public class Cylinder extends Tube {
+    private double _height;
+    public Cylinder(Ray axisRay, double radius, double height) {
+        super(axisRay, radius);
+        if (height <= 0)
+            throw new IllegalArgumentException("height has to be positive");
+       _height = height;
+    }
+    public Vector getNormal(Point point) {
+        return null;
+    }
+    @Override
+    public String toString() {
+        return "Cylinder: " + "height: " + _height + "axisRay: " + _axisRay + "radius: " + _radius;
+    }
+
 }
