eaac647416d4445c53ab19f6b45b3ce1665183b0
Tehila321 <100318288+Tehila321@users.noreply.github.com>
2022-03-06 20:56:49 +0200

    Update Tube.java

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -3,9 +3,17 @@ import java.util.List;
 
 import primitives.*;
 import static primitives.Util.*;
-
+/**
+ * class for representing a tube
+ */
 public class Tube implements Geometry {
+    /**
+     * tube direction
+     */
     protected final Ray axisRay;
+    /**
+     * tube radius
+     */
     protected final double radius;
 
     public Tube(Ray _axisRay, double _radius) {
