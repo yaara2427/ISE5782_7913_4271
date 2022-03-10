ef068c7261df51152d0d6b23867ffbacb2d484da
Tehila321 <treichma@g.jct.ac.il>
2022-03-10 18:37:07 +0200

    hi

--- src/primitives/VectorTest.java
+++ src/primitives/VectorTest.java
@@ -1,70 +1,86 @@
 package primitives;
 
+import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
+
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.AssertJUnit.assertTrue;
+import static primitives.Util.isZero;
 
 class VectorTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void testDotProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(-2, -4, -6);
+        Vector v3 = new Vector(0, 3, -2);
+
+        // =============== Boundary Values Tests ==================
+        assertTrue(isZero(v1.dotProduct(v3)), "ERROR: dotProduct() for orthogonal vectors is not zero");
+
+        // ============ Equivalence Partitions Tests ==============
+        assertTrue(isZero(v1.dotProduct(v2) + 28), "ERROR: dotProduct() wrong value");
+    }
+    @Test
     void testAdd() {
-    }
+            // ============ Equivalence Partitions Tests ==============
+            // TC01: There is a simple single test here
+            Vector v1 = new Vector(1, -5, 20);
+            Vector v2 = new Vector(-8, 3, 10);
+            assertEquals(v1.add(v2),new Vector(-7,-2,30),"ERROR: add() wrong value");
+        }
+        @Test
+        void testSubtract() {
+            // ============ Equivalence Partitions Tests ==============
+            // TC01: There is a simple single test here
+            Vector v1 = new Vector(17, 5, 2);
+            Vector v2 = new Vector(-7, 4, 8);
+            assertEquals(v1.subtract(v2),new Vector(24,1,-6),"ERROR: subtract() wrong value");
+        }
 
-    @org.junit.jupiter.api.Test
-    void testSubtract() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void testScale() {
     }
 
-    @org.junit.jupiter.api.Test
-    void testDotProduct() {
-    }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testCrossProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(-2, -4, -6);
+        Vector v3 = new Vector(0, 3, -2);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testLengthSquared() {
+        Vector v1 = new Vector(1, 2, 3);
+        assertTrue(isZero(v1.lengthSquared()-14),"ERROR: lengthSquared() wrong value");
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testLength() {
+        assertTrue(isZero(new Vector(0, 3, 4).length()-5),"ERROR: length() wrong value");
     }
 
-    @org.junit.jupiter.api.Test
+
+    @Test
     void testNormalize() {
     }
-
-    @org.junit.jupiter.api.Test
-    void testTestAdd() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testTestSubtract() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testTestScale() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testTestDotProduct() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testTestCrossProduct() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testTestLengthSquared() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testTestLength() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testTestNormalize() {
-    }
 }
+if (!isZero(v1.lengthSquared() - 14))
+        out.println("ERROR: lengthSquared() wrong value");
+        if (!isZero(new Vector(0, 3, 4).length() - 5))
+        out.println("ERROR: length() wrong value");
+
+// test Cross-Product
+        try { // test zero vector
+                v1.crossProduct(v2);
+                out.println("ERROR: crossProduct() for parallel vectors does not throw an exception");
+                } catch (Exception e) {
+                }
+                Vector vr = v1.crossProduct(v3);
+                if (!isZero(vr.length() - v1.length() * v3.length()))
+                out.println("ERROR: crossProduct() wrong result length");
+                if (!isZero(vr.dotProduct(v1)) || !isZero(vr.dotProduct(v3)))
+                out.println("ERROR: crossProduct() result is not orthogonal to its operands");
