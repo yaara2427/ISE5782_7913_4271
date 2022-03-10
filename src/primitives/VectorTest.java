06394038b1b46acab0839f230614f34831878c0a
Tehila321 <treichma@g.jct.ac.il>
2022-03-10 13:57:52 +0200

    hi

---
+++ src/primitives/VectorTest.java
@@ -0,0 +1,70 @@
+package primitives;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSubtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testScale() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDotProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testCrossProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testLengthSquared() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testNormalize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestSubtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestScale() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestDotProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestCrossProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestLengthSquared() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestNormalize() {
+    }
+}
