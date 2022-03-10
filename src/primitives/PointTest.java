6c659dd12c5b7420bd9cf7388b82f81747f6bf75
Tehila321 <treichma@g.jct.ac.il>
2022-03-10 13:56:56 +0200

    hi

---
+++ src/primitives/PointTest.java
@@ -0,0 +1,38 @@
+package primitives;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSubtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDistanceSquared() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDistance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestSubtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestDistanceSquared() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestDistance() {
+    }
+}
