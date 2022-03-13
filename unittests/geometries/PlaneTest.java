b0336161711f4f5375baf70d7a12015ba274dba3
yaara2427 <yaara2427@gmail.com>
2022-03-13 22:53:21 +0200

    ff

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -14,20 +14,19 @@ class PlaneTest {
 
         //TC01: 2 points coinciding
         assertThrows(IllegalArgumentException.class,
-                () -> new Plane(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 0, 1)),
+                () -> new Plane(new Point(1, 2, 3), new Point(1, 2, 3), new Point(4, 5, 6)),
                 "Failed constructing a correct plane");
 
         //TC02: The 3 points are on one line
         assertThrows(IllegalArgumentException.class,
-                () -> new Plane(new Point(0, 0, 1), new Point(0, 0, 2), new Point(0, 0, 3)),
+                () -> new Plane(new Point(1, 2, 3), new Point(2, 4, 6), new Point(3, 6, 9)),
                 "Failed constructing a correct plane");
     }
     @Test
     void testGetNormal() {
         // ============ Equivalence Partitions Tests ==============
         // TC01: There is a simple single test here
-        Plane pl = new Plane(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0));
-        double sqrt3 = Math.sqrt(1d / 3);
-        assertEquals(new Vector(sqrt3, sqrt3, sqrt3), pl.getNormal(new Point(0, 0, 1)), "Bad normal to plane");
+        Plane pl = new Plane(new Point(1, 0, 0), new Point(1, 1, 0), new Point(0, 1, 0));
+        assertEquals(new Vector(0, 0, 1), pl.getNormal(new Point(0, 0, 1)), "Bad normal to plane");
     }
 }
